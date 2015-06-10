902889b1c4f6073e1736309a1a7cf0fde4cdc1e3
Justin Hill <justinmhill@gmail.com>
2015-06-10 00:37:35 -0400

    Add function to get random word from list

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
+
+var getWord = function(wordList) {
+	var index = Math.floor(Math.random() * wordList.length);
+	return wordList[index];
+};
+
 io.on('connection', function(socket) {
 	console.log('New client connected');
 	socket.on('draw', function(position) {
