7183367419e27b31589891d272cbcb12ef84a86e
Justin M. Hill <justinmhill@gmail.com>
2016-05-18 11:48:57 -0400

    Move socket.io events to own module

--- server.js
+++ server.js
@@ -5,37 +5,10 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-var game = require('./game');
+var socketEvents = require('./socketEvents');
 
 app.use(express.static('public'));
 
-io.on('connection', function(socket) {
-	var word = game.getWord();
-	console.log('New client connected');
-
-	socket.on('draw', function(position) {
-		socket.broadcast.emit('draw', position);
-	});
-
-	socket.on('guess', function(guess) {
-		socket.broadcast.emit('guess', guess);
-	});
-
-	socket.on('claim pen', function(){
-		socket.drawer = true;
-		socket.emit('drawer', word);
-		socket.broadcast.emit('pen claimed');
-	});
-
-	socket.on('disconnect', function() {
-		if (socket.drawer) {
-			console.log('A drawer disconnected');
-			socket.broadcast.emit('pen open');
-		} else {
-			console.log('A guesser disconnected');
-		}
-	});
-});
+io.on('connection', socketEvents);
 
 server.listen(port);
