640f4db30fc1f01b3ebc9a0d74449ee99f5938e4
Justin M. Hill <justinmhill@gmail.com>
2016-05-18 12:47:54 -0400

    Reorganize main.js

--- public/main.js
+++ public/main.js
@@ -1,98 +1,101 @@
 var pictionary = function() {
-	var canvas, 
-		context,
-		drawer, 
-		drawing, 
-		guessbox;
-
 	var socket = io();
 
-	var draw = function (position) {
+	var canvas = $('canvas');
+	var context = canvas[0].getContext('2d');
+
+	var isInDrawMode = false;
+	var isDrawing = false;
+
+	canvas[0].width = canvas[0].offsetWidth;
+	canvas[0].height = canvas[0].offsetHeight;
+
+	function draw(position) {
 		context.beginPath();
 		context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
 		context.fill();
 	};
 
-	var onKeyDown = function (event) {
+	function clearCanvas() {
+		context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+	}
+
+	function displayWord(word) {
+		isInDrawMode = true;
+		$('#word').text('You\'re the drawer. Draw a ' + word + '!').css('display', 'block');
+		$('#guess').hide();
+	}
+
+	function onKeyDown(event) {
 		if (event.keyCode != 13) { // If NOT enter/return key
 			return;
 		}
 
-		var guess = guessbox.val();
+		var guess = $('#guess').val();
 		console.log(guess);
 		socket.emit('guess', guess);
 		addGuess(guess);
-		guessbox.val('');
+		$('#guess').val('');
 	};
 
-	var addGuess = function(guess) {
-		var guesses = guessList.text();
-		guessList.text(guesses + guess + ', ');
+	function addGuess(guess) {
+		var guesses = $('#guess-list').text();
+		$('#guess-list').text(guesses + guess + ', ');
 	};
 
-	canvas = $('canvas');
-	context = canvas[0].getContext('2d');
-	canvas[0].width = canvas[0].offsetWidth;
-	canvas[0].height = canvas[0].offsetHeight;
-	
+	// JQuery Listeners
+
+	$('#clear').on('click', clearCanvas);
+
+	$('#guess').on('keydown', onKeyDown);
+
 	canvas.on('mousedown', function() {
-		if (drawer) {
-			drawing = true;
+		if (isInDrawMode) {
+			isDrawing = true;
 		}
 	});
 
 	canvas.on('mouseup', function() {
-		drawing = false;
+		isDrawing = false;
 	});
 
 	canvas.on('mousemove', function(event) {
 		var offset = canvas.offset();
-		var position = { 
+		var position = {
 			x: event.pageX - offset.left,
 			y: event.pageY - offset.top
 		};
-		if (drawing) {
+		if (isDrawing) {
 			draw(position);
 			socket.emit('draw', position);
 		}
 	});
 
+	$('#claim').on('click', function() {
+		socket.emit('claim pen');
+		$(this).hide();
+	});
+
+	// Socket.io listeners
+
 	socket.on('draw', function(position) {
 		draw(position);
 	});
 
-	clearButton = $('#clear');
-	clearButton.on('click', function() {
-		context.clearRect(0, 0, canvas[0].width, canvas[0].height);
-	});
-
-	guessbox = $('#guess').find('input');
-	guessbox.on('keydown', onKeyDown);
-
-	guessList = $('#guess-list');
 	socket.on('guess', addGuess);
 
-	claimButton = $('#claim').find('button');
-	claimButton.on('click', function() {
-		socket.emit('claim pen');
-		claimButton.hide();
-	});
-
 	socket.on('pen claimed', function() {
-		claimButton.hide();
+		$('#claim').hide();
 	});
 
 	socket.on('pen open', function() {
-		claimButton.show();
+		$('#claim').show();
+		$('#guesses').hide();
 	});
-	
-	wordToDraw = $('#word');
-	socket.on('drawer', function(word) {
-		drawer = true;
-		wordToDraw.text('You\'re the drawer. Draw a ' + word + '!').css('display', 'block');
-		$('#guess').hide();
-	});
-};
+
+	socket.on('drawer', displayWord);
+
+}; // End pictionary()
 
 $(document).ready(function(){
 	pictionary();
