3bebf8dcbc6f0167f746152d293c5ac61602fdc8
Justin M. Hill <justinmhill@gmail.com>
2016-05-18 15:23:01 -0400

    Set up initial Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,140 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    // Dev Tool Configurations
+    env: {
+      dev: {
+        NODE_ENV: 'development'
+      },
+      test: {
+        NODE_ENV: 'test'
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          ext:'js, html',
+          watch: [
+            'server.js'
+          ]
+        }
+      },
+      debug: {
+        script: 'server.js',
+        options: {
+          nodeArgs: ['--debug'],
+          ext: 'js, html',
+          watch: [
+            'server.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: [
+          'server.js'
+        ],
+        tasks: ['jshint']
+      },
+      src: {
+        files: ['src/**/*.js'],
+        tasks: ['browserify']
+      },
+      css: {
+        files: ['public/css/*.css'],
+        tasks: ['csslint']
+      }
+    },
+
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      },
+      debug: {
+        tasks: ['nodemon:debug', 'watch', 'node-inspector'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+
+
+    'node-inspector': {
+      debug: {
+        options: {
+          'web-port': 8989
+        }
+      }
+    },
+
+    // Build Configurations
+    browserify: {
+      dist: {
+        options: {
+          transform: ["babelify"]
+        },
+        files: {
+          "./public/js/app.js": ["./src/client/app.js"]
+        }
+      }
+    },
+
+    // Testing Configurations
+    mochaTest: {
+      src: 'test/**/*.js',
+      options: {
+        reporter: 'spec'
+      }
+    },
+
+    // Linting Configurations
+    jshint: {
+      all: {
+        src: ['server.js'],
+        options: {
+          esnext: true
+        }
+      }
+    },
+    csslint: {
+      all: {
+        src: 'public/css/*.css'
+      }
+    }
+
+  });
+
+  // Register Tasks
+  grunt.registerTask('default', [
+    'env:dev',
+    'lint',
+    'browserify',
+    'concurrent:dev'
+  ]);
+
+  grunt.registerTask('debug', [
+    'env:dev',
+    'lint',
+    'browserify',
+    'concurrent:debug'
+  ]);
+
+  grunt.registerTask('test', [
+    'env:test',
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('lint', [
+    'jshint',
+    'csslint'
+  ]);
+}
