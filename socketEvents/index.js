e41e139edb94338bf8122b87fc8109ea57931fd0
Justin M. Hill <justinmhill@gmail.com>
2016-07-12 20:02:39 -0400

    Refactor socket.io usage to keep logic at top of comp. hierarchy
    As a rule, it is best to keep most of the application logic in a
    container component near the top level of the component tree. This
    has the benefit of minimizing the number of stateful components in
    order to ensure that the application is easier to reason about.
    
    In this particular case, we register all of our socket event
    listeners in the top level App component, and then use various
    functions to ensure that the correct data is passed down to child
    components.
    
    This separation of concerns should allow for easier future
    exploration of using systems like Redux to handle changes
    to application state in a more sensible way.
    
    Closes #27

--- socketEvents/index.js
+++ socketEvents/index.js
@@ -1,31 +1,38 @@
 var game = require('../game-engine');
 
 module.exports = function(socket) {
-	var word = game.getWord();
-	console.log('New client connected');
+  
+  console.log('New client connected');
+  
+  // Initialize a new game
+  socket.on('game:start', function() {
+    var word = game.getWord();
+    socket.emit('game:start', word, true);
+    socket.broadcast.emit('game:start', null, false);
+  });
+  
+  // Server recceives a new guess
+  socket.on('game:guess', function(guess) {
+    socket.broadcast.emit('game:guess', guess);
+  })
+  
+  // Server receives drawing data
+  socket.on('game:draw', function(position) {
+    socket.broadcast.emit('game:draw', position);
+  });
 
-	socket.on('draw', function(position) {
-		socket.broadcast.emit('draw', position);
-	});
-
-	socket.on('guess', function(guess) {
-		socket.broadcast.emit('guess', guess);
-		console.log('user just guessed' + guess);
-	});
-
-	socket.on('claim pen', function(){
-		socket.isInDrawMode = true;
-		socket.emit('drawer', word);
-		socket.broadcast.emit('pen claimed');
-		console.log('user asked for pen control');
-	});
-
-	socket.on('disconnect', function() {
-		if (socket.isInDrawMode) {
-			console.log('A drawer disconnected');
-			socket.broadcast.emit('pen open');
-		} else {
-			console.log('A guesser disconnected');
-		}
-	});
+  // If a player drops, needs to handle sending updates to other players:
+  // If drawing player drops, need to end game
+  // If other player, just notify
+  // TODO: This still has the old socket implementation, update as new features added
+  // For example, when user leaves, send out a socket event notifying other clients
+  // It won't break anything as is, so just leave it for now until user list feature added
+  socket.on('disconnect', function() {
+    if (socket.isInDrawMode) {
+      console.log('A drawer disconnected');
+      socket.broadcast.emit('pen open');
+    } else {
+      console.log('A guesser disconnected');
+    }
+  });
 };
